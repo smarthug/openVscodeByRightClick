58ccbe2e83d580df608e4cd708786ffd19f7be3f
smarthug <kirklayer@gmail.com>
2021-07-31 19:28:50 +0900

    now it works

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-// const repoUrlPath = '/' + location.pathname.split('/').slice(1, 3).join('/');
-
-function main(){
-  
-  chrome.contextMenus.create(
-    {
-      title: 'Go to "open.vscode.dev' + location.pathname + '"',
-      id: location.pathname,
-      documentUrlPatterns:["https://github.com/*/*"]
-    },
-  );
-
-  chrome.contextMenus.onClicked.addListener(function (itemData) {
-    // console.log(window.location.href)
-    window.open("https://open.vscode.dev"+ location.pathname, "_blank");
+function main() {
+  chrome.contextMenus.create({
+    title: "Go to open.vscode.dev of this repository",
+    id: location.pathname,
+    documentUrlPatterns: ["https://github.com/*/*"],
   });
 
-
-  
-
-
+  chrome.contextMenus.onClicked.addListener(function (itemData) {
+    chrome.tabs.getSelected(null, function (tab) {
+      window.open("https://open.vscode.dev/" + tab.title, "_blank");
+    });
+  });
 }
 
 main();
