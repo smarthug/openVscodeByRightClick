395c78fe94f2305ea5a79bdf59678a0eccd20314
hosuk <kirklayer@gmail.com>
2021-08-02 10:16:44 +0900

    regex rather than tab title

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function main() {
+  const regEx = RegExp(/\/\w+\/\w+/ , 'g')
+
   chrome.contextMenus.create({
     title: "Go to open.vscode.dev of this repository",
     id: location.pathname,
@@ -7,7 +9,7 @@ function main() {
 
   chrome.contextMenus.onClicked.addListener(function (itemData) {
     chrome.tabs.getSelected(null, function (tab) {
-      window.open("https://open.vscode.dev/" + tab.title, "_blank");
+      window.open("https://open.vscode.dev/" + regEx.exec(tab.url)[0], "_blank");
     });
   });
 }
